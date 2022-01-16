bc1b2114b5d58e2de016fd830a6ab54cd56c591c
RVerma90 <irahulverma90@gmail.com>
2022-01-16 13:34:47 +0000

    feat(payroll-date): replace dummy date with salary and bonus dates based on start date selection

--- utils/payroll-calculator.ts
+++ utils/payroll-calculator.ts
@@ -4,11 +4,19 @@ export function getPayrollDates(dateValue: string): PayrollDate[] {
   const dateObject: Date = new Date(dateValue)
 
   let allDates: PayrollDate[] = []
-  for(let i = 1; i <= 12; i++) {
-    
+  for(let i = 0; i <= 11; i++) {
+    // TODO: optimisation opportunity for handling end of month edge cases (29th, 30th)
+    const currentMonth = new Date(dateObject.getFullYear(), dateObject.getMonth() + i, dateObject.getDate())
+
+    // end of month date (/the day before the first of next month)
+    const currentMonthSalaryDate = new Date((new Date(currentMonth.getFullYear(), currentMonth.getMonth() + 1, 1)) - 1);
+
+    // 15th of following month
+    const currentMonthBonusDate = new Date(currentMonth.getFullYear(), currentMonth.getMonth() + 1, 15);
+
     const dates = {
-      salaryDate: `Dummy salary ${i}`,
-      bonusDate: `Dummy bonus ${i}`,
+      salaryDate: currentMonthSalaryDate.toString().slice(0,15),
+      bonusDate: currentMonthBonusDate.toString().slice(0,15),
     }
     
     allDates.push(dates)
